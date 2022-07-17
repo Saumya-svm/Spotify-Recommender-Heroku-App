e612c93c1b99c202523eb249997057ef7599dc5c
Saumya Mundra <saumyamundra@gmail.com>
2022-07-18 01:09:24 +0530

    fixed flashes

--- spotify/routes.py
+++ spotify/routes.py
@@ -152,7 +152,7 @@ def spotifyaccount():
 def spotifylogout():
     session['access_token'] = ''
     flash('**User logged out')
-    session['img'] = session['img'] = '../static/profile_pics/default.jpg'
+    session['img'] = '../static/profile_pics/default.jpg'
     # return redirect('https://accounts.spotify.com/en/logout')
     return render_template('home.html')
 
